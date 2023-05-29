cb7ec4ca7764226f554c33bdfe137ed60bf12198
dilly <dillyprashad211997@gmail.com>
2023-05-29 22:11:12 +0530

    cell setup and IB conections

---
+++ WisdomLeaf/WisdomLeaf/View/ImageCell.swift
@@ -0,0 +1,27 @@
+//
+//  ImageCell.swift
+//  WisdomLeaf
+//
+//  Created by DDP on 29/05/23.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+
+    @IBOutlet weak var baseView: UIView!
+    @IBOutlet weak var rolledImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- WisdomLeaf/WisdomLeaf/ViewController.swift
+++ WisdomLeaf/WisdomLeaf/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageTable.delegate = self
+        imageTable.dataSource = self
+        imageTable.register(UINib(nibName: "ImageCell", bundle: nil), forCellReuseIdentifier: "ImageCell")
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = imageTable.dequeueReusableCell(withIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        cell.rolledImage.image = UIImage(named: "dummyimage")
+        return cell
+
+    }
+    
+    
+}
