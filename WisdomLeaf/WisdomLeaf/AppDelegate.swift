6cb4b8521b1b5ac53ff4041e2a6e958436afa45e
mj <96232962+dilly2197@users.noreply.github.com>
2023-05-29 20:38:53 +0530

    initial commit

---
+++ WisdomLeaf/WisdomLeaf/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  WisdomLeaf
+//
+//  Created by DDP on 29/05/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window:UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
